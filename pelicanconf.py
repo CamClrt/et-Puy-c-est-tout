474de056ec8d0355f74f0f95eeac4912f61d2101
CamClrt <camille.clarret@gmail.com>
2022-05-09 06:53:04 +0200

    imp settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,5 +27,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 5
 
+RELATIVE_URLS = True
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
